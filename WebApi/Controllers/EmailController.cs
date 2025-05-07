3722dd042cfd8c6f04306c6564033eb3b6f30146
Nino Hägglund <ninomh99@gmail.com>
2025-05-07 20:27:28 +0200

    Add WebApi project and email service functionality
    - Added new "WebApi" project to the solution, removing "EmailService.Api".
    
    - Created `EmailController` for handling email requests.
    
    - Introduced `EmailSendRequest` model for email data.
    
    - Implemented `EmailService` class using Azure Communication Services.
    
    - Updated `WebApi.csproj` to include Azure Communication Email package.

---
+++ WebApi/Controllers/EmailController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController(EmailService emailService) : ControllerBase
+{
+}
---
+++ WebApi/Models/EmailSendRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models;
+
+public class EmailSendRequest
+{
+    //lista med mottagare
+    public List<string> Recipients { get; set; } = null!;
+    public string Subject { get; set; } = null!;
+    public string Body { get; set; } = null!;
+    public string Html { get; set; } = null!;
+  
+}
